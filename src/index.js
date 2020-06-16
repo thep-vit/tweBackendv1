4654c1aa0be7d882ef16662f2a9ef485eae7ca8b
AbhinavGor <56104674+AbhinavGor@users.noreply.github.com>
2020-06-16 11:19:16 +0530

    Image upload functionality
    Added image upload functionality with multer and GridFS.
    Tried adding flash messages with connect-flash but didn't get any
    progress there.
    The image upload was giving a Type Error of not being able to read
    the property readPreference of undefined. So, I downgraded mongoose
    to v4.13 to solve the issue.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-require("./db/mongo")
+const conn = require("./db/mongo")
 
 
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const path = require("path")
 const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const userRouter = require("./routes/users")
 const articleRouter = require("./routes/articles")
 const indexRouter = require("./routes/index")
-const resetRouter = require("./routes/reset")
+const resetRouter = require("./routes/reset");
+const { connect } = require("http2");
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -24,10 +27,27 @@ const partialsPath = path.join(__dirname,"./views/partials")
 // Set View Engine
 app.use(expressLayouts);
 app.set("view engine", "ejs");
-app.set("views",viewsPath)
+app.set("views",viewsPath);
+
 
 // Static assests are served from public directory - css, js etc
 app.use(express.static("public"))
+
+//Flash messages
+app.use(cookieParser('secretString'));
+app.use(flash());
+app.use(session({
+	secret:'happy dog',
+	saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: 60000 }
+}));
+
+app.use(function(req, res, next){
+    res.locals.success_messages = req.flash('success_messages');
+    res.locals.error_messages = req.flash('error_messages');
+    next();
+});
 
 
 // Cookies
--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,27 +1,138 @@
-const express = require("express")
-const Article = require("../models/articles")
-const auth = require("../middleware/auth")
+const express = require("express");
+const Article = require("../models/articles");
+const Item = require('../models/items');
+const fs = require('fs');
+const auth = require("../middleware/auth");
+const flash = require('connect-flash');
+
+
+const crypto = require('crypto');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const multer = require("multer");
+const path = require('path');
+const mongoose = require('mongoose');
+const { route } = require("./users");
+
+//Init gfs
+const mongoURI = 'mongodb://127.0.0.1:27017/twe';
+
+const conn = mongoose.createConnection(mongoURI,{useNewUrlParser:true});
+
+
+let gfs;
+
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+  console.log("connection made successfully");
+});
+
+
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
 
 const router = new express.Router()
 
 // POST new article
-router.post("/",auth, async(req,res)=>{
-    console.log("Before Post Article")
-    console.log("req body",req.body)
+router.post("/",auth, upload.single('file'), async(req,res)=>{
+    // console.log("Before Post Article")
+    // console.log("req body",req.body)
     const newArticle = new Article({
         ...req.body,
         author: req.user._id
     })
     
     try {
-        await newArticle.save()
-        res.redirect("/users/dashboard")
+        await newArticle.save();
+        res.locals.message = req.body.message;
+        res.redirect("/users/dashboard").json( { message: 'your message' });
         // res.status(201).send(newArticle)
     } catch (e) {
         res.status(400).send()
     }
+
 })
 
+
+//@route GET /articles/files
+//@desc Display details of all files 
+router.get('/files', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+        //Check if files
+        if(!files || files.length === 0){
+            res.status(404).json({
+                err: 'No files exist'
+            });
+        }
+
+        //Files exist
+        return res.json(files);
+    })
+})
+
+// //@route GET /articles/files/:filename
+// //@desc Display single file 
+// router.get('/files/:filename', (req, res) =>{
+//     gfs.files.findOne({ filename: req.params.filename}, (err, file) => {
+//         //check if file
+//         if(!file || file.length === 0){
+//             return res.status(404).json({
+//                 err: 'No file found'
+//             });
+//         }
+//         //File exists
+//         return res.json(file);
+//     });
+// });
+
+//@route GET /articles/image/:filename
+//@desc Display image with filename 
+router.get('/files/:filename', (req, res) =>{
+
+    gfs.files.findOne({ filename: req.params.filename}, (err, file) => {
+        //check if file
+        if(!file || file.length === 0){
+            return res.status(404).json({
+                err: 'No file found'
+            });
+        }
+        //File exists
+        if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
+            
+            // res.set('Content-Type', file.contentType);
+            // res.set('Content-Disposition', 'attachment; filename="' + file.filename + '"');
+
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.on('error', function (err) {
+                console.error('Read error', err);
+                res.end();
+              });
+            readstream.pipe(res);
+        }else{
+            res.status(404).json({
+                err: 'File not found'
+            })
+        }
+    });
+});
 
 // GET /tasks?limit=2&skip=2
 // GET /tasks?sortBy=createdAt:asc
