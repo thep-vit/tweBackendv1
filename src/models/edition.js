9da5c27a3b7894f6055a03532b395c19b9d2e25a
Sanjit Kumar <sanjitk2018@gmail.com>
2020-06-25 16:12:27 +0530

    edition collection and virtual links to articles and fixed earlier bugs
    - created edition model
    - POST /edition/create to create new edtion
    - GET /edition/:number to get edtion details
    - PATCH /articles/select/edition/:id to approve and select edition route
    - changed contribution updation from mongoose model middleware to inside routes (Should move to express middleware)

---
+++ src/models/edition.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+
+const editionSchema = mongoose.Schema({
+    ename:{
+        type:String,
+        required: true,
+        trim: true
+    },
+    enumber: {
+        type: Number,
+        required:true,
+        unique:true,
+        validate(value){
+            if (value<0){
+                throw new Error("Edition Number cannot be negetive")
+            }
+        }
+    },
+    // hov: {
+    //     type: mongoose.SchemaTypes.URL
+    // },
+    // earticles:[{
+    //     earticle: {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: 'Article'
+    //     }
+    // }]
+},{
+    timestamps: true
+})
+
+
+//establishing a relationshipt between edition and articles
+
+editionSchema.virtual( "articles", {  
+    ref: "Article",
+    localField: "_id",
+    foreignField : "edition"
+})
+
+const Edition = mongoose.model('Edition',editionSchema)
+
+module.exports = Edition
