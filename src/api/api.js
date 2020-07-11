c710bac0ce8b90164fe5dfb6df4e68a3ae3f7b46
AbhinavGor <56104674+AbhinavGor@users.noreply.github.com>
2020-07-11 14:17:38 +0530

    Removed image upload

--- src/api/api.js
+++ src/api/api.js
@@ -300,12 +300,12 @@ const upload = multer({
 router.post("/articles",auth, upload.single("picture"), async(req,res)=>{
     // console.log("Before Post Article")
     console.log("req body",req.body)
-    const buffer = await sharp(req.file.buffer).resize({ height: 250, width: 250}).png().toBuffer()
+    // const buffer = await sharp(req.file.buffer).resize({ height: 250, width: 250}).png().toBuffer()
     
     const newArticle = new Article({
         ...req.body,
         author: req.user._id,
-        picture: buffer
+        // picture: buffer
     })
     
     try {
