83e59b75d7f48e32a138f980f0623f1cdedfbc61
AbhinavGor <56104674+AbhinavGor@users.noreply.github.com>
2020-07-17 12:24:50 +0530

    Comsole logging req.file

--- src/api/api.js
+++ src/api/api.js
@@ -300,6 +300,7 @@ const upload = multer({
 router.post("/articles",auth, upload.single("picture"), async(req,res)=>{
     // console.log("Before Post Article")
     console.log("req body",req.body)
+    console.log("req.file: ", req.file);
     const buffer = await sharp(req.file.buffer).resize({ height: 250, width: 250}).png().toBuffer()
     
     const newArticle = new Article({
