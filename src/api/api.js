8a90891823ba2e8b40e1d4d29564529fc06df3be
AbhinavGor <56104674+AbhinavGor@users.noreply.github.com>
2020-07-27 17:28:26 +0530

    Removed resizing for images

--- src/api/api.js
+++ src/api/api.js
@@ -11,9 +11,14 @@ const Article = require("../models/articles")
 const Edition = require("../models/edition")
 
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'abhinavgorantla0613@gmail.com',
+      pass: 'kjvebhnpgijovmpt'
+    }
+  });
 
-// Dynamic Rendering with EJS Templating Engine 
-// GET requests that render the pages
 
 router.get("", (req,res)=> {
     // res.render("index",{
@@ -31,7 +36,7 @@ router.post("/users/signup", async (req,res) => {
     const newUser = new User(req.body)
     try{
         // console.log(req.body)
-        // console.log("Register Route")
+        // console.log("Register Route")       
         await newUser.save()
         const token = await newUser.generateToken()
 
@@ -149,14 +154,6 @@ router.delete("/users/me", auth, async (req,res) => {
 // @route POST api/users/recover
 // @desc Recover Password - Generates token and Sends password reset email
 // @access Public
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'abhinavgorantla0613@gmail.com',
-      pass: 'kjvebhnpgijovmpt'
-    }
-  });
-
 router.post('/users/recover',  async (req, res) => {
     try {
         foundUser = await User.findOne({email: req.body.email})
@@ -241,6 +238,14 @@ check('confirmPassword', 'Passwords do not match').custom((value, {req}) => (val
 
 });
 
+// @route   POST api/users/confirmed
+// @desc    Comfirms email for user.
+// @access Public
+router.post('/users/confirmed/:id', (req, res) => {
+    const id = req.params.id;
+    
+})
+
 // Contributions
 
 router.get("/users/me/contribution", auth, async (req,res)=>{
@@ -299,7 +304,7 @@ const upload = multer({
 // create article
 router.post("/articles",auth, upload.single("picture"), async(req,res)=>{    
     try {
-        const buffer = await sharp(req.file.buffer).resize({ height: 250, width: 250}).png().toBuffer()
+        const buffer = await sharp(req.file.buffer).png().toBuffer()
     
         const newArticle = new Article({
             ...req.body,
