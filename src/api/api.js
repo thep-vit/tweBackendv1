241b5b422ba49e8c106e575d19c9a0fab9d3b10d
AbhinavGor <abhinav20016@gmail.com>
2020-11-09 17:15:00 +0530

    Removed pic

--- src/api/api.js
+++ src/api/api.js
@@ -559,7 +559,7 @@ router.patch("/articles/select/edition/:id", auth, adminAuth, async(req,res)=>{
 // Get all existing articles
 router.get("/admin/allarticles",auth, async (req,res)=>{
     try{
-        const allarticles = await Article.find({})
+        const allarticles = await Article.find({}).select("-picture")
         if (!allarticles){
             throw new Error()
         }
