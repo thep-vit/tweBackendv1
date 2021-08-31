16089b8722a3a58be65c0f96a368860b7771407c
AbhinavGor <abhinav20016@gmail.com>
2021-08-31 09:41:21 +0530

    ADD: user profile routes

--- src/api/api.js
+++ src/api/api.js
@@ -933,6 +933,47 @@ router.get('/messages/allMessages', async (req, res) => {
 
 })
 
+router.get('/profile/:id', async (req, res) => {
+    try {
+        const userID = req.params.id
+        const foundUser = await User.findOne({ _id: userID});
+    
+        if(foundUser){
+            if(!foundUser.profile){
+                res.status(408).send({"message" : `No profile found for user with email ${foundUser.email}!`});
+            }else{
+                res.status(200).send(foundUser.profile);
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+    }
+});
+
+router.post('/profile/:id', auth, async (req, res) => {
+    try {
+        const userID = req.params.id;
+        const foundUser = await User.findOne({ _id: userID});
+        const { name, avatar, bio } = req.body;
+
+        foundUser.profile = {
+            avatar, bio
+        }
+
+        await foundUser.save();
+
+        console.log(foundUser);
+
+
+        res.status(200).send(foundUser);
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+    }
+});
 
 
 module.exports = router
--- src/routes/articles.js
+++ src/routes/articles.js
@@ -22,8 +22,6 @@ const upload = multer({
 
 // POST new article
 router.post("/",auth, upload.single("picture"), async(req,res)=>{
-    // console.log("Before Post Article")
-    // console.log("req body",req.body)
     const buffer = await sharp(req.file.buffer).resize({ height: 250, width: 250}).png().toBuffer()
     
     const newArticle = new Article({
